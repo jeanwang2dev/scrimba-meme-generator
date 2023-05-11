40d859aef1129262203ccc58dc35b3357bceb5bb
jeanwang2dev <jeanharmsen@yahoo.com>
2023-05-10 20:46:42 -0400

    make topText and bottomText in meme state

--- src/App.js
+++ src/App.js
@@ -1,39 +1,12 @@
-import { useState } from 'react';
-import boxes from "./boxes"
-import Box from "./components/Box"
+import Header from "./components/Header";
+import Meme from "./components/Meme";
 
 const App = () => {
-    const [squares, setSquares] = useState(boxes)
     
-    function toggle(id) {
-        //console.log(id)
-        setSquares( prevSquares => {
-            // const index = prevSquares.findIndex( item => item.id === id)
-            return prevSquares.map( item => {
-                let currentItem = {...item}
-                if(item.id === id) {
-                    currentItem.on = !item.on
-                }
-                return currentItem
-            })
-        })
-    }
-
-    const squareElements = squares.map(square => (
-        <Box 
-            key={square.id} 
-            id={square.id}
-            on={square.on} 
-            toggle={toggle}
-        />
-    ))
-
     return (
-        <div className="max-w-md mx-auto">
-            <section className="flex flex-wrap">
-                {squareElements}
-            </section>
-            
+        <div className="max-w-xl mx-auto">
+            <Header />
+            <Meme />            
         </div>
     )
 }
--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -11,6 +11,15 @@ export default function Meme () {
 
     const [allMemeImage, setAllMemeImage] = useState(memesData) 
 
+    function handleChange(event){
+        const { name, value} = event.target
+        setMeme( prevMeme => ( {
+            ...prevMeme,
+            [name]:value
+            }
+        ))
+    }
+
     function getMemeImage(){
         const memesArr = allMemeImage.data.memes;
         const randomMeme = memesArr[Math.floor(Math.random() * memesArr.length)];
@@ -27,11 +36,30 @@ export default function Meme () {
     return (
        <main className="p-9">
             <div className="grid grid-rows-2 grid-cols-2 gap-4">
-                <input className="border border-gray-300 px-3 py-2 font-normal text-xs" type="text" placeholder="Top Text" required/>
-                <input className="border border-gray-300 px-3 py-2 font-normal text-xs" type="text" placeholder="Bottom Text" required/>
+                <input 
+                    className="border border-gray-300 px-3 py-2 font-normal text-xs" 
+                    type="text" 
+                    placeholder="Top Text" 
+                    value={meme.topText}
+                    name="topText"
+                    onChange={handleChange}
+                    required/>
+                <input 
+                    className="border border-gray-300 px-3 py-2 font-normal text-xs" 
+                    type="text" 
+                    placeholder="Bottom Text" 
+                    value={meme.bottomText}
+                    name="bottomText"
+                    onChange={handleChange}
+                    required/>
                 <button onClick={getMemeImage} className="col-span-2 text-white text-base font-bold bg-gradient-to-r from-startColor from-2% to-endColor to-100% cursor-pointer">Get a new meme image🖼</button>
             </div>
-            <img className="mt-7" src={meme.randomImage} alt="" />
+            <div className="relative">
+               <img className="mt-7" src={meme.randomImage} alt="" />
+               <div className="absolute top-2 flex justify-center w-full"><h2 className="memeText">{meme.topText}</h2></div> 
+               <div className="absolute bottom-2 flex justify-center w-full"><h2 className="memeText">{meme.bottomText}</h2></div>
+            </div>
+            
        </main>
     );
 }
